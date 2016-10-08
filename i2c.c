5aabc11f149897cd1645166106fb554c6b729c39
Kimmo Lindholm <kimmo@eke.fi>
2016-10-08 14:17:53 +0300

    fix accelerometer interface
    multiple byte read and write does not autoincrement register address  :nail_care:

--- i2c.c
+++ i2c.c
@@ -18,9 +18,14 @@ const I2CConfig i2cconfig =
 float getExtTemperature(void)
 {
     uint8_t rxBuf[2];
+    msg_t ret;
 
+    i2cAcquireBus(&I2CD1);
     i2cMasterTransmit(&I2CD1, TK_I2C_LM75_ADDR, (uint8_t[]){LM75_TEMPREG}, 1, NULL, 0);
-    if (MSG_OK == i2cMasterReceive(&I2CD1, TK_I2C_LM75_ADDR, rxBuf, sizeof(rxBuf)))
+    ret = i2cMasterReceive(&I2CD1, TK_I2C_LM75_ADDR, rxBuf, sizeof(rxBuf));
+    i2cReleaseBus(&I2CD1);
+
+    if (ret == MSG_OK)
     {
         extTempOK = 1;
         return (float)(int16_t)( (rxBuf[0] << 8) | (rxBuf[1] & 0xE0) ) / 256;
@@ -35,14 +40,23 @@ float getExtTemperature(void)
 void getAcceleration(void)
 {
     uint8_t rxBuf[6];
+    msg_t ret = MSG_OK;
+    int i;
 
-    i2cMasterTransmit(&I2CD1, TK_I2C_IIS328_ADDR, (uint8_t[]){IIS328_OUTREG}, 1, NULL, 0);
-    if (MSG_OK == i2cMasterReceive(&I2CD1, TK_I2C_IIS328_ADDR, rxBuf, sizeof(rxBuf)))
+    i2cAcquireBus(&I2CD1);
+    for (i=0 ; i<6; i++)
+    {
+        i2cMasterTransmit(&I2CD1, TK_I2C_IIS328_ADDR, (uint8_t[]){IIS328_OUTREG+i}, 1, NULL, 0);
+        ret |= i2cMasterReceive(&I2CD1, TK_I2C_IIS328_ADDR, rxBuf+i, 1);
+    }
+    i2cReleaseBus(&I2CD1);
+
+    if (ret == MSG_OK)
     {
         accelOK = 1;
-        accelX = (float)(int16_t)( (rxBuf[0]<<8) | rxBuf[1] ) * IIS328_8G_SCALE;
-        accelY = (float)(int16_t)( (rxBuf[2]<<8) | rxBuf[3] ) * IIS328_8G_SCALE;
-        accelZ = (float)(int16_t)( (rxBuf[4]<<8) | rxBuf[5] ) * IIS328_8G_SCALE;
+        accelX = (float)(((int16_t)( (rxBuf[0]<<8) | rxBuf[1]))/16) * IIS328_8G_SCALE;
+        accelY = (float)(((int16_t)( (rxBuf[2]<<8) | rxBuf[3]))/16) * IIS328_8G_SCALE;
+        accelZ = (float)(((int16_t)( (rxBuf[4]<<8) | rxBuf[5]))/16) * IIS328_8G_SCALE;
     }
     else
     {
@@ -57,7 +71,7 @@ void i2cTKInit(void)
 {
     i2cStart(&I2CD1, &i2cconfig);
 
-    uint8_t txBuf[6];
+    uint8_t txBuf[2];
 
     accelOK = 0;
     extTempOK = 0;
@@ -66,12 +80,13 @@ void i2cTKInit(void)
     accelY = (float)0.0;
     accelZ = (float)0.0;
 
+    /* Initialize accelerometer */
+    i2cAcquireBus(&I2CD1);
     txBuf[0] = IIS328_CTRL1REG;
     txBuf[1] = IIS328_CTRL1REG_VALUE;
-    txBuf[2] = IIS328_CTRL2REG_VALUE;
-    txBuf[3] = IIS328_CTRL3REG_VALUE;
-    txBuf[4] = IIS328_CTRL4REG_VALUE;
-    txBuf[5] = IIS328_CTRL5REG_VALUE;
-
-    i2cMasterTransmit(&I2CD1, TK_I2C_IIS328_ADDR, txBuf, 6, NULL, 0);
+    i2cMasterTransmit(&I2CD1, TK_I2C_IIS328_ADDR, txBuf, 2, NULL, 0);
+    txBuf[0] = IIS328_CTRL4REG;
+    txBuf[1] = IIS328_CTRL4REG_VALUE;
+    i2cMasterTransmit(&I2CD1, TK_I2C_IIS328_ADDR, txBuf, 2, NULL, 0);
+    i2cReleaseBus(&I2CD1);
 }
--- i2c.h
+++ i2c.h
@@ -7,10 +7,8 @@
 #define TK_I2C_IIS328_ADDR 0x18
 #define IIS328_CTRL1REG 0x20
 #define IIS328_CTRL1REG_VALUE 0xC7 /* Low power 10Hz, 37Hz LPF, XYZ enabled */
-#define IIS328_CTRL2REG_VALUE 0x00 /* Normal mode, HP filter bypassed */
-#define IIS328_CTRL3REG_VALUE 0x00 /* Interrupts not used, defaults are ok */
+#define IIS328_CTRL4REG 0x23
 #define IIS328_CTRL4REG_VALUE 0xF0 /* No update between reads, MSB, 8g, selftest disabled */
-#define IIS328_CTRL5REG_VALUE 0x00 /* Sleep to wake disabled */
 #define IIS328_OUTREG 0x28
 #define IIS328_8G_SCALE (0.00391)
 
