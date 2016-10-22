f317e3d9825daf0c6c47ff913307d112807ddf78
Kimmo Lindholm <kimmo@eke.fi>
2016-10-22 22:51:27 +0300

    rework on tcp server

--- simplelink/platform/tcp_server.c
+++ simplelink/platform/tcp_server.c
@@ -14,6 +14,31 @@ static int16_t connSockId = 0;
 static const char *welcomeMessage = "Welcome to " BOARD_NAME "\n\n\r";
 static const char *goodbyeMessage = "Bye!\n\r";
 
+#if 0
+static int setNonBlockingMode(int mode)
+{
+    int res;
+    SlSockNonblocking_t nonBlockingOption;
+
+    nonBlockingOption.NonblockingEnabled = mode;
+    res = sl_SetSockOpt(sockId, SOL_SOCKET, SL_SO_NONBLOCKING, &nonBlockingOption, sizeof(nonBlockingOption));
+
+    return res;
+}
+#endif
+
+static int setReceiveTimeout(int ms)
+{
+    int res;
+    struct SlTimeval_t timeVal;
+
+    timeVal.tv_sec = ms / 1000;
+    timeVal.tv_usec = (ms % 1000) * 1000;
+    res = sl_SetSockOpt(sockId, SL_SOL_SOCKET, SL_SO_RCVTIMEO, (uint8_t *)&timeVal, sizeof(timeVal));
+
+    return res;
+}
+
 static int createSocket(uint16_t port)
 {
     SlSockAddrIn_t sServerAddress;
@@ -22,30 +47,26 @@ static int createSocket(uint16_t port)
     sockId = sl_Socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     if (sockId > 0)
     {
-        DEBUG("Socket created.\n\r");
-
-        /* make socket non-blocking by setting 1 sec timeout */
-        struct SlTimeval_t timeVal;
-        timeVal.tv_sec = 5;
-        timeVal.tv_usec = 0;
-        sl_SetSockOpt(sockId, SL_SOL_SOCKET, SL_SO_RCVTIMEO, (uint8_t *)&timeVal, sizeof(timeVal));
+        setReceiveTimeout(1000);
 
         sServerAddress.sin_family = AF_INET;
         sServerAddress.sin_addr.s_addr = INADDR_ANY;
         sServerAddress.sin_port = sl_Htons(port);
 
         res = sl_Bind(sockId, (const SlSockAddr_t *)&sServerAddress, sizeof(sServerAddress));
-        DEBUG("bind %d\n\r", res);
 
         res |= sl_Listen (sockId, 1);
-        DEBUG("listen %d\n\r", res);
 
         if (res == SL_SOC_OK)
         {
-            DEBUG("listening port %d\n\r", port);
+            PRINT("TCP Terminal listening on port %d\n\r", port);
             return MSG_OK;
         }
-        sl_Close(sockId);
+        else
+        {
+            sl_Close(sockId);
+            return MSG_RESET;
+        }
     }
 
     return MSG_RESET;
@@ -53,11 +74,12 @@ static int createSocket(uint16_t port)
 
 static int waitForConnection(void)
 {
-    SlSocklen_t  in_addrSize;
-    sockaddr_in  sClientAddress;
+    SlSocklen_t in_addrSize;
+    SlSockAddrIn_t sClientAddress;
 
-    // accepts a connection from a TCP client, if there is any, otherwise returns SL_EAGAIN
+    in_addrSize = sizeof(sClientAddress);
     connSockId = sl_Accept(sockId, (SlSockAddr_t *)&sClientAddress, (SlSocklen_t *)&in_addrSize);
+
     if (connSockId == SL_EAGAIN)
     {
         return SL_EAGAIN;
@@ -66,16 +88,16 @@ static int waitForConnection(void)
     {
         if (connSockId <= 0)
         {
-            DEBUG("waitForConnection error %d\n\r", connSockId);
+            DEBUG("error %d\n\r", connSockId);
             return MSG_RESET;
         }
-
-        sl_Close(sockId);
     }
 
+    uint32_t clientAddr = sl_Htonl(sClientAddress.sin_addr.s_addr);
+
     PRINT("TCP Terminal client connected from %d.%d.%d.%d\n\r",
-        SL_IPV4_BYTE(sClientAddress.sin_addr.s_addr, 3), SL_IPV4_BYTE(sClientAddress.sin_addr.s_addr, 2),
-        SL_IPV4_BYTE(sClientAddress.sin_addr.s_addr, 1), SL_IPV4_BYTE(sClientAddress.sin_addr.s_addr, 0));
+        SL_IPV4_BYTE(clientAddr, 3), SL_IPV4_BYTE(clientAddr, 2),
+        SL_IPV4_BYTE(clientAddr, 1), SL_IPV4_BYTE(clientAddr, 0));
 
     return MSG_OK;
 }
@@ -111,8 +133,8 @@ static int receiveFromSocket(void *buff, _i16 Maxlen, _i16 *rxLen)
         return SL_EAGAIN;
     else if (*rxLen > 0)
         return MSG_OK;
-    else
-        return MSG_RESET;
+
+    return MSG_RESET;
 }
 
 static THD_FUNCTION(tcpTermServer, arg)
@@ -120,55 +142,68 @@ static THD_FUNCTION(tcpTermServer, arg)
     (void) arg;
     int res;
 
-    while (true)
+    res = createSocket(23);
+    if (res == MSG_RESET)
     {
-        res = createSocket(23);
-        if (res == MSG_RESET)
-        {
-            PRINT("Failed to create socket\n\r");
-        }
-        else
-        {
-            while ((res = waitForConnection()) == SL_EAGAIN)
-            {
-                chThdSleepMilliseconds(100);
-                if (res == MSG_RESET)
-                {
-                    PRINT("Failed while waiting for connection\n\r");
-                }
-            }
-
-            if (res == MSG_OK)
-            {
-                sendToSocket((char *) welcomeMessage, strlen(welcomeMessage));
-
-                while (res != MSG_RESET)
-                {
-                    int16_t rxLen;
-                    chThdSleepMilliseconds(20);
-
-                    res = receiveFromSocket(rxBuff, BUFSIZE, &rxLen);
-
-                    if (res == MSG_OK)
-                    {
-                        /* Just dump the data out */
-                        dump(rxBuff, (int)rxLen);
-                        if (strncmp(rxBuff, "exit", 4) == 0)
-                        {
-                            sendToSocket((char *) goodbyeMessage, strlen(goodbyeMessage));
-                            break;
-                        }
-                    }
-                }
-
-                PRINT("Connection closed\n\r");
-                sl_Close(connSockId);
-            }
-        }
-
-        chThdSleepMilliseconds(500);
+        PRINT("Failed to create socket\n\r");
+        chHeapFree(rxBuff);
+        chThdExit(MSG_OK);
     }
 
+    while (true)
+    {
+        //setNonBlockingMode(true);
+
+        do
+        {
+            chThdSleepMilliseconds(100);
+
+            res = waitForConnection();
+
+            if (res == MSG_RESET)
+            {
+                PRINT("Failed while waiting for connection\n\r");
+            }
+        }
+        while (res == SL_EAGAIN);
+
+        if (res == MSG_OK)
+        {
+            //setNonBlockingMode(false);
+
+            sendToSocket((char *) welcomeMessage, strlen(welcomeMessage));
+
+            while (res != MSG_RESET)
+            {
+                int16_t rxLen;
+
+                chThdSleepMilliseconds(20);
+
+                res = receiveFromSocket(rxBuff, BUFSIZE, &rxLen);
+
+                if (res == MSG_OK)
+                {
+                    /* Just dump the data out */
+                    dump(rxBuff, (int)rxLen);
+                    if (strncmp(rxBuff, "exit", 4) == 0)
+                    {
+                        sendToSocket((char *) goodbyeMessage, strlen(goodbyeMessage));
+                        break;
+                    }
+                }
+                else if (res == MSG_RESET)
+                {
+                    break;
+                }
+            }
+
+            PRINT("Connection closed\n\r");
+        }
+        sl_Close(connSockId);
+        chThdSleepMilliseconds(100);
+    }
+
+    sl_Close(sockId);
     chHeapFree(rxBuff);
     chThdExit(MSG_OK);
 }
