08ecdb830898d24700a68f5b875a37ccc7efbb86
Kimmo Lindholm <kimmo@eke.fi>
2016-11-05 20:37:32 +0200

    i2c bus reset

--- i2c.c
+++ i2c.c
@@ -16,6 +16,7 @@ const I2CConfig i2cconfig =
 
 static void updateExtTemperature(void);
 static void updateAcceleration(void);
+static bool i2cBusReset(void);
 
 static int i2cEnabled;
 
@@ -92,6 +93,8 @@ void updateAcceleration(void)
 
 void i2cTKInit(void)
 {
+    while (!i2cBusReset()) { }
+
     i2cStart(&I2CD1, &i2cconfig);
     chBSemObjectInit(&i2cReadyReadSem, true);
 
@@ -115,4 +118,57 @@ void i2cTKInit(void)
 void startI2cThread(void)
 {
     chThdCreateFromHeap(NULL, THD_WORKING_AREA_SIZE(256), "i2c", NORMALPRIO+1, i2cThread, NULL);
+}
+
+/*
+ * I2C Bus Reset - Analog Devices AN-686 Solution 1.
+ */
+
+bool i2cBusReset(void)
+{
+    int count = 0;
+    bool ret = false;
+
+    /* Configure I2C pins to GPIO */
+    palSetPadMode(GPIOB, GPIOB_PB6_I2C1SCL, PAL_STM32_MODE_OUTPUT | PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_HIGHEST);
+    palSetPadMode(GPIOB, GPIOB_PB7_I2C1SDA, PAL_STM32_MODE_OUTPUT | PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_HIGHEST);
+    chThdSleepMilliseconds(1);
+
+    /* Try to make signals high */
+    palSetPad(GPIOB, GPIOB_PB6_I2C1SCL);
+    palSetPad(GPIOB, GPIOB_PB7_I2C1SDA);
+    chThdSleepMilliseconds(1);
+
+    /* If SDA is low, clock SCL until SDA is high */
+    while (palReadPad(GPIOB, GPIOB_PB7_I2C1SDA) == PAL_LOW && count < 50)
+    {
+        palClearPad(GPIOB, GPIOB_PB6_I2C1SCL);
+        chThdSleepMilliseconds(1);
+        palSetPad(GPIOB, GPIOB_PB6_I2C1SCL);
+        chThdSleepMilliseconds(1);
+        count++;
+    }
+
+    /* Generate a stop condition */
+    palClearPad(GPIOB, GPIOB_PB6_I2C1SCL);
+    chThdSleepMilliseconds(1);
+    palClearPad(GPIOB, GPIOB_PB7_I2C1SDA);
+    chThdSleepMilliseconds(1);
+    palSetPad(GPIOB, GPIOB_PB6_I2C1SCL);
+    chThdSleepMilliseconds(1);
+    palSetPad(GPIOB, GPIOB_PB7_I2C1SDA);
+    chThdSleepMilliseconds(1);
+
+    /* Return true, if SDA is now high */
+    ret = (palReadPad(GPIOB, GPIOB_PB7_I2C1SDA) == PAL_HIGH);
+
+    palSetPadMode(GPIOB, GPIOB_PB6_I2C1SCL, PAL_STM32_MODE_ALTERNATE | PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_HIGHEST | PAL_STM32_ALTERNATE(4));
+    palSetPadMode(GPIOB, GPIOB_PB7_I2C1SDA, PAL_STM32_MODE_ALTERNATE | PAL_STM32_OTYPE_OPENDRAIN | PAL_STM32_OSPEED_HIGHEST | PAL_STM32_ALTERNATE(4));
+
+    if (count > 0)
+        PRINT(" - I2C was stuck, and took %d cycles to reset.\n\r", count);
+
+    chThdSleepMilliseconds(10);
+
+    return ret;
 }
